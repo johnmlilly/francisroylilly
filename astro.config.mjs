a586f721a534b89301dc651de7c6e6377d52731e
John Lilly <jm2lilly@gmail.com>
2025-10-31 14:26:13 -0400

    add comment database, component, and functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import db from '@astrojs/db';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), react()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), db()],
+  output: 'server',
+  adapter: netlify(),
 });
