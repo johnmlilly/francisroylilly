512eaadfc0de9b4644d12c923dd7ae24d3337d84
John Lilly <jm2lilly@gmail.com>
2025-11-12 13:53:25 -0500

    correct class name

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -13,7 +13,7 @@ export default function ShareButton() {
   return (
     <button
   onClick={handleCopy}
-  class="flex items-center gap-1 bg-white rounded-lg px-4 py-3 shadow-sm border border-gray-100 w-fit mb-4 hover:bg-gray-50 transition"
+  className="flex items-center gap-1 bg-white rounded-lg px-4 py-3 shadow-sm border border-gray-100 w-fit mb-4 hover:bg-gray-50 transition"
 >
   <Forward size={21} /> {copied ? "Copied!" : "Share"}
 </button>
