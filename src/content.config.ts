d35ff6a9288dc23a681081249f74a01d71efa56c
John Lilly <jm2lilly@gmail.com>
2026-02-13 20:35:42 -0500

    use astro image component for rendering

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
-			galleryPhotos: z.array(z.string()).optional()
+			galleryPhotos: z.array(image()).optional()
 		}),
 });
 
