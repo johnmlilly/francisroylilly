8c80294dc1dbbcdbcaeb1f2d59887d17940fb2f3
John Lilly <jm2lilly@gmail.com>
2026-09-02 21:15:33 -0400

    update sitewide meta

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Francis Roy';
-export const SITE_DESCRIPTION = 'Documenting Francis Roy\'s journey.';
+export const SITE_TITLE = 'Francis Roy Lilly | Our Little Lion';
+export const SITE_DESCRIPTION = 'Sharing the story of how God saved our son. Read his journey, how we leaned on our faith and community, and how he\'s doing today.'
 export const SITE_LOGO = 'src/assets/images/francis-roy-logo.svg'
